ddcd4d4fe6264d40c73d63ade416ba03112f5584
Gerard Amoros Serra <geramse@gmail.com>
2018-12-10 22:30:59 +0200

    test

--- Game.java
+++ Game.java
@@ -1,10 +1,22 @@
 package game;
+import java.util.Scanner;
 
 public class Game {
 
 	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in); 
 		// TODO Auto-generated method stub
 			System.out.println("HelloWorld");
+			int n = reader.nextInt();
+			System.out.println(n);
+			
+			
+			
+			
+			
+			
+			
+			reader.close();
 	}
 
 }
